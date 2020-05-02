ffa9a8e53a8dedc0dc52d9136aec15e815351c0d
MedikLash <64482818+MedikLash@users.noreply.github.com>
2020-05-02 17:04:14 +0300

    Add files via upload

---
+++ js/game.js
@@ -0,0 +1,54 @@
+const numDivs = 36;
+const maxHits = 10;
+
+let hits = 0;
+let firstHitTime = 0;
+
+function round() {
+  // FIXME: надо бы убрать "target" прежде чем искать новый "Готово"
+  $('.target').removeClass("target");
+  //$('.miss').removeClass("miss");
+  let divSelector = randomDivId();
+  $(divSelector).addClass("target");
+  // TODO: помечать target текущим номером "Готово"
+  $(divSelector).text(hits+1);
+  // FIXME: тут надо определять при первом клике firstHitTime "Готово"
+  if (hits == 1) {
+    firstHitTime = getTimestamp();
+  }
+  if (hits === maxHits) {
+    endGame();
+  }
+}
+
+function endGame() {
+  // FIXME: спрятать игровое поле сначала "Готово"
+  $("#game_filde").addClass("d-none");
+  let totalPlayedMillis = getTimestamp() - firstHitTime;
+  let totalPlayedSeconds = Number(totalPlayedMillis / 1000).toPrecision(3);
+  $("#total-time-played").text(totalPlayedSeconds);
+
+  $("#win-message").removeClass("d-none");
+}
+
+function handleClick(event) {
+  // FIXME: убирать текст со старых таргетов. Кажется есть .text? "Готово"
+  $('.target').text("");
+  if ($(event.target).hasClass("target")) {
+    hits = hits + 1;
+    round();
+  }
+  // TODO: как-то отмечать если мы промахнулись? См CSS класс .miss
+}
+
+function init() {
+  // TODO: заказчик просил отдельную кнопку, запускающую игру а не просто по загрузке
+  round();
+
+  $(".game-field").click(handleClick);
+  $("#button-reload").click(function() {
+    location.reload();
+  });
+}
+
+$(document).ready(init);
